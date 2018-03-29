55da8fb6ad347c35183ded35a209f09905c40a1e
Thiago Pereira Martins <thiago.martins@nddigital.local>
2018-03-29 15:33:44 -0300

    mysql configurado

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,12 +1,12 @@
 import * as fs from 'fs';
 import * as path from 'path';
 import * as Sequelize from 'sequelize';
+import { DbConnection } from '../interfaces/DbConnectionInterface';
 
 const basename: string = path.basename(module.filename);
 const env: string = process.env.NODE_ENV || 'development';
-let config = require(path.resolve(`${__dirname}./../config/config.json`))[env];
+let config = require(path.resolve(`${__dirname}./../config/config.json`))[env.trim()];
 let db = null;
-
 if (!db) {
     db = {};
 
